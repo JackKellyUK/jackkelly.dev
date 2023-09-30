e1850f0aeed9e71e33acb9db7fa71aa3bce36f2f
Jack Kelly <mail@jackkelly.dev>
2023-09-30 18:21:18 +0100

    Fixed movie border

--- components/movie/card/card.js
+++ components/movie/card/card.js
@@ -17,7 +17,7 @@ const Card = ({ data, addToWatchList, inWatchList }) => {
               <div className="relative">
                 <img src={"https://image.tmdb.org/t/p/w500" + data.poster_path} />
                 <p className="absolute top-1.5 right-1.5 bg-gray-700 rounded-md aspect-square text-sm w-8 grid place-items-center drop-shadow-lg font-semibold">{ data.vote_average.toFixed(1) }</p>
-                <button onClick={e => addToWatchList(e, data)} className={"absolute bottom-1.5 right-1.5 w-6 p-1 rounded-full border-white border-2" + (inWatchList ? " text-amber-400 border-amber-400" : "")}><FontAwesomeIcon icon={faStar} /></button>
+                <button onClick={e => addToWatchList(e, data)} className={"absolute bottom-1.5 right-1.5 w-6 p-1 rounded-full border-2" + (inWatchList ? " text-amber-400 border-amber-400" : " border-white")}><FontAwesomeIcon icon={faStar} /></button>
               </div>
               <div className="px-4 py-3">
                   <h3 className="leading-tight mb-1">
